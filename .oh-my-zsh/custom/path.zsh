e5b5766f39e3374cb9638bef74a183d2cac28b61
mimovnik <mimovnik@protonmail.com>
2024-02-24 14:04:42 +0100

    Add need-ssh bin to path

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -2,5 +2,6 @@ export PATH="$HOME/adb-fastboot/platform-tools:$PATH"
 
 export PATH="$PATH:/opt/nvim-linux64/bin"
 export PATH="$PATH:/home/deck/.local/bin"
+export PATH="$PATH:$HOME/.need-ssh/bin"
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
