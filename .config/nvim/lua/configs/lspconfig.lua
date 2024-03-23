554cd049aa98d54dbfc47946c9b1b8a58f291985
mimovnik <mimovnik@protonmail.com>
2024-03-23 17:36:28 +0100

    Configure clang lsp for c

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,8 +4,8 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
 
+local servers = { "clangd" }
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
