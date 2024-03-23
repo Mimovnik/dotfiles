9e6f3fd0c5e55c10f52e64b6ea2f228f122b37e9
mimovnik <mimovnik@protonmail.com>
2024-03-23 17:55:37 +0100

    Unmap ESC from closing a floating terminal

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,9 +1,14 @@
 require "nvchad.mappings"
 
--- add yours here
-
 -- local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local nomap = vim.keymap.del
+
+-- Disable closing a terminal with ESC
+-- It was causing a scenario where
+-- it was impossible to exit insert mode from vim spawned inside
+-- the terminal (ex. when entering a git commit message)
+nomap("t", "<ESC>")
